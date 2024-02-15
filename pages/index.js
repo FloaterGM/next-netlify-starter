20c5d2e39083ebbb031809d4de35e9c7caeeb180
Cristobal Valencia Hernandez <vcristobal3@gmail.com>
2024-02-14 23:58:20 -0500

    cambios

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,44 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <style>{`
+          .heart {
+            position: relative;
+            width: 100px;
+            height: 90px;
+            transform: rotate(-45deg);
+            margin: 50px auto;
+          }
+          .heart:before,
+          .heart:after {
+            position: absolute;
+            content: "";
+            left: 50px;
+            top: 0;
+            width: 50px;
+            height: 80px;
+            background-color: red;
+            border-radius: 50px 50px 0 0;
+            transform: rotate(-45deg);
+            transform-origin: 0 100%;
+          }
+          .heart:after {
+            left: 0;
+            transform: rotate(45deg);
+            transform-origin: 100% 100%;
+          }
+          .message {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 20px;
+          }
+        `}</style>
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div className="heart"></div>
+        <div className="message">Te quiero mucho cielo</div>
       </main>
 
       <Footer />
